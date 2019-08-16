7d43352a2fa54a37ff147af50a965f0fc7902e9c
NnaDid <chrispneuma@gmail.com>
2019-08-16 03:28:40 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,412 @@
+<!doctype html>
+
+<html>
+
+   <head>
+    <title>midwa</title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel ="stylesheet" 	href ="css/font-awesome.css" />
+	<link rel ="stylesheet" 	href ="css/main.css" />
+	<link rel ="shortcut icon"  href ="images/logo.png"/>
+
+   </head>
+   
+    <body>
+     <div class ="bg-light container-fluid main">
+	
+	      	<nav style ="border-bottom:1px solid #70AD47; text-align:right;">
+			<a class="btn btn-outline-success btn-sm" href="#">Register</a>
+			<a class="btn btn-outline-success btn-sm" href="#">&nbsp;Login &nbsp;</a>
+				<a class="btn" href="#">English</a>
+				<a class="btn" href="#">French</a>
+				<a class="btn" href="#">Português</a>
+			</nav>
+
+
+		<nav class="navbar navbar-expand-lg navbar-light bg-light" style ="border-bottom:2px solid #70AD47;">
+		<div class="collapse navbar-collapse col-md-12" id="navbarTogglerDemo01">
+			<a class="navbar-brand" href="#">
+			<img src="images/logo.png" class ="img-responsive" width ="150" height ="150" style ="border-radius:4em;"/>
+			<span class ="bold hidden-xs-down">Migration Dialogue For West Africa</span>
+			</a>
+			<ul class="navbar-nav ml-auto" >
+			  <li class="nav-item">
+				<form>
+			    <div class="input-group mb-3">
+				  <input type="text" class="form-control" placeholder="Search">
+				  <div class="input-group-append">
+					<button class="btn btn-outline-success"><i class ="fa fa-search"></i></button>
+				  </div>
+				</div>
+				</form>
+			  </li>
+			</ul>
+			</div>
+		</nav>
+
+
+	
+		<nav class="navbar navbar-expand-lg navbar-light bg-light mb-2" style ="border-bottom:2px solid #70AD47;">
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse " id="navbarTogglerDemo01">
+			<ul class="navbar-nav mr-auto mt-0 mt-lg-0 navBtns">
+			  <li class="nav-item dropdown">
+				<a class="nav-link">About <i class ="fa fa-caret-down"></i></a>
+				  <div class="dropdown-content">
+				  <a class="" href="#">Brief History of MIDWA</a>
+				  <a class="" href="#">Mission</a>
+				  <a class="" href="#">Vision</a>
+				  <a class="" href="#">Core Values</a>
+				  </div>
+			 </li>
+			  <li class="nav-item dropdown">
+				<a class="nav-link" href="#">Thematic areas 
+				<i class ="fa fa-caret-down"></i></a>
+				<div class="dropdown-content">
+				  <a class="" href="#">Labour Migration</a>
+				  <a class="" href="#">Migration Data</a>
+				  <a class="" href="#">Migration Environment</a>
+				  <a class="" href="#">Migration and Development</a>
+				  <a class="" href="#">Mixed Migration/Protection</a>
+				  </div>
+			  </li>
+
+
+
+			  <li class="nav-item dropdown">
+				<a class="nav-link" href="#">Resources<i class ="fa fa-caret-down"></i></a>
+				<div class="dropdown-content">
+				  <a class="" href="#">Publications</a>
+				  <a class="" href="#">Articles</a>
+				  </div>
+			  </li>
+			  <li class="nav-item dropdown">
+				<a class="nav-link" href="#">Meetings</a>
+			  </li> 
+			  <li class="nav-item dropdown">
+				<a class="nav-link" href="#">Media<i class ="fa fa-caret-down"></i></a>
+				<div class="dropdown-content">
+				  <a class="" href="#">Videos</a>
+				  <a class="" href="#">Pictures</a>
+				  </div>
+			  </li>
+			  <li class="nav-item dropdown">
+				<a class="nav-link" href="#">Contact</a>
+			  </li>
+			</ul>
+		  </div>
+		</nav>
+	
+	
+						 
+			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style ="height:300px;">
+			  <ol class="carousel-indicators">
+				<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+			  </ol>
+			  <div class="carousel-inner  mt-2"  style ="height:300px;">
+				<div class="carousel-item active">
+				  <img src="images/1.jpg" class="d-block w-100" alt="...">
+				</div>
+				<div class="carousel-item">
+				  <img src="images/2.jpg" class="d-block w-100" alt="...">
+				</div>
+				<div class="carousel-item">
+				  <img src="images/3.jpg" class="d-block w-100" alt="...">
+				</div>
+			  </div>
+			 <!--   
+			 
+			  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			  </a>
+			  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			  </a>
+			 -->
+	   </div>
+			
+			
+			
+<div class ="container-fluid mt-4 mb-4">	
+     <div class ="bg-gradient-success text-center mt-4 mb-4 py-6"> 
+	 <h4> MIDWA INTERVENTION AREAS</h4>
+	 </div>		
+   <div class ="row mt-2 mb-2">	
+			<!--  Left pane --->		
+	 <div class ="col-md-12">
+  	      <div class ="row">	
+			<!--  Intervention Area--->	
+			<div class ="col-md-4">	
+			<div class="card" style="border-color:#70AD47;">
+			  <div class="card-body">
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" class="card-link">Card link</a>
+			  </div>
+			</div>
+			</div>
+			<div class ="col-md-4">	
+			<div class="card" style="border-color:#70AD47;">
+			  <div class="card-body">
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" class="card-link">Card link</a>
+			  </div>
+			</div>
+			</div>
+			<div class ="col-md-4">	
+			<div class="card" style="border-color:#70AD47;">
+			  <div class="card-body">
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" class="card-link">Card link</a>
+			  </div>
+			</div>
+			</div>
+			
+		</div>	<div class ="row mt-2">	
+			<!--  Intervention Area--->	
+			<div class ="col-md-4">	
+			<div class="card" style="border-color:#70AD47;">
+			  <div class="card-body">
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" class="card-link">Card link</a>
+			  </div>
+			</div>
+			</div>
+			<div class ="col-md-4">	
+			<div class="card" style="border-color:#70AD47;">
+			  <div class="card-body">
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" class="card-link">Card link</a>
+			  </div>
+			</div>
+			</div>
+			<div class ="col-md-4">	
+			<div class="card" style="border-color:#70AD47;">
+			  <div class="card-body">
+				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" class="card-link">Card link</a>
+			  </div>
+			</div>
+			</div>
+			
+		</div>
+				   
+	</div>
+ 			
+  </div>
+</div>	
+	
+	
+	
+
+    <!-- NEWS  -->
+	<div class ="mb-4">
+			<div class="card">
+			  <div class="card-body">
+			  <div class ="bg-gradient-success text-center mt-4 mb-4 py-6">
+			  <h4> NEWS </h4>
+			  </div>
+				<p class="card-text">
+					<ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
+					  <li class="nav-item">
+						<a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Latest News</a>
+					  </li>
+					  <li class="nav-item">
+						<a class="nav-link" id="profile-tab" data-toggle="tab" href="#upcomingEvent" role="tab" aria-controls="profile" aria-selected="false">Upcoming Events</a>
+					  </li>
+					  <li class="nav-item">
+						<a class="nav-link" id="profile-tab" data-toggle="tab" href="#archive" role="tab" aria-controls="profile" aria-selected="false">Archive</a>
+					  </li>
+					</ul>
+					<div class="tab-content" id="myTabContent">
+					  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+					    Between 6-7 August, local entrepreneur Rachidatou Abdou organized a two-day training on homemade skincare products for more than 40 young girls and women at IOM’s transit center for migrant women in Niamey, as they waited for their return to their country of origin through the Assisted Voluntary Return and Reintegration (AVRR) programme, under the EU-IOM Joint Initiative for Migrant Protection and Reintegration.  
+
+						Rachidatou, 29, decided to create her enterprise Foyer Annour Aicha in 2016 when she had a hard time finding a job in Niamey. Since she had always been passionate about cosmetics, Rachidatou opened a business selling homemade skincare products such as soaps, body lotions and perfumes.  
+
+						Since becoming an IDEE beneficiary in January 2019, Rachidatou has improved the packaging of her products and her marketing skills, and delivered training sessions for women in Niger, Gabon, Senegal and Burkina Faso. Once back in their countries of origin, the women at the transit center can use their newly acquired skills as a starting point for an income-generating activity. 
+											  </div>
+					  
+				  <div class="tab-pane fade" id="upcomingEvent" role="tabpanel" aria-labelledby="contact-tab">
+						 <ul class="list-unstyled">
+						  <li class="media">
+							<img src="images/21.jpg" class="mr-3" alt="...">
+							<div class="media-body">
+							  <h5 class="mt-0 mb-1">List-based media object</h5>
+							  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+							</div>
+						  </li>
+						  <li class="media my-4">
+							<img src="images/21.jpg" class="mr-3" alt="...">
+							<div class="media-body">
+							  <h5 class="mt-0 mb-1">List-based media object</h5>
+							  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+							</div>
+						  </li>
+						  <li class="media">
+							<img src="images/21.jpg" class="mr-3" alt="...">
+							<div class="media-body">
+							  <h5 class="mt-0 mb-1">List-based media object</h5>
+							  Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+							</div>
+						  </li>
+						</ul>
+				  </div>
+				  
+				  <div class="tab-pane fade show active" id="archive" role="tabpanel" aria-labelledby="home-tab">
+					   News Archive
+				  
+					</div>
+				
+				</p>
+			  </div>
+			</div>
+
+	</div>
+	
+	
+	
+	<div class ="row jumbotron justify-content-md-center flags">
+		    <div  class ="col-md-4">
+			    <ul style ="list-style:none;">
+				     <li>
+				<a href="https://www.ecowas.int/member-states/benin/">
+						<img width ="75" height ="75" src="flags/benin.jpg" alt="benin">				
+						BENIN
+						</a>
+				</li>
+						
+							
+				<li>
+				<a href="https://www.ecowas.int/member-states/burkina-faso/">
+						<img width ="75" height ="75" src="flags/burkinafaso.jpg" alt="burkinafaso">				
+						BURKINA FASO				</a>
+				</li>
+						
+							
+				<li><a href="https://www.ecowas.int/member-states/cabo-verde/">
+						<img width ="75" height="75" src="flags/caboverde.jpg" alt="Carbo Verde">				
+						CABO VERDE	
+						</a>
+				</li>
+				
+				
+				<li><a href="https://www.ecowas.int/member-states/liberia/">
+						<img width ="75" height="75" src="flags/liberia.jpg" alt="Liberia">				
+						LIBERIA				</a>
+				</li>
+						
+							
+				<li><a href="https://www.ecowas.int/member-states/mali/">
+						<img width ="75" height ="75" src="flags/mali.jpg" alt="Mali">				
+						MALI				</a>
+				</li>
+				
+				  </ul>
+			</div>
+			
+			
+			
+			 <div  class ="col-md-4">
+			      <ul style ="list-style:none;">
+				     							
+				<li><a href="https://www.ecowas.int/member-states/cote-divoire/">
+						<img width ="75" height ="75" src="flags/codevoire.jpg"alt="cote-divoire">				
+						CÔTE D'IVOIRE				</a>
+				</li>
+						
+							
+				<li><a href="https://www.ecowas.int/member-states/gambia/">
+						<img width ="75" height ="75" src="flags/TheGambia.jpg" alt="Gambia">				
+						The GAMBIA				</a>
+				</li>
+						
+							
+				<li><a href="https://www.ecowas.int/member-states/ghana/">
+						<img width ="75" height ="75" src="flags/ghana.jpg"  alt="Ghana">				
+						GHANA </a>
+				</li>
+				<li><a href="https://www.ecowas.int/member-states/guinea/">
+						<img width ="75" height ="75" src="flags/guinea.jpg" alt="Guinea">				
+						GUINEA </a>
+				</li>
+						
+							
+				<li><a href="https://www.ecowas.int/member-states/guinee-bissau/">
+				    <img width ="75" height ="75" src="flags/guineaBissau.jpg" alt="guinee-bissau">				
+						GUINEA BISSAU </a>
+				</li>
+				
+			   </ul>
+			</div>
+			
+			<ul>
+
+										
+			<div  class ="col-md-4">
+				<ul style ="list-style:none;">		
+							
+				<li><a href="https://www.ecowas.int/member-states/niger/">
+						<img width ="75" height="75" src="flags/niger.jpg" alt="Niger">				
+						NIGER </a></li>
+						
+							
+				<li><a href="https://www.ecowas.int/member-states/nigeria/">
+						<img width ="75" height ="75" src="flags/nigeria.jpg" alt="Nigeria">				
+						NIGERIA	</a></li>
+						
+							
+				<li><a href="https://www.ecowas.int/member-states/senegal/">
+						<img width ="75" height ="60" src="flags/senegal.jpg" alt="Senegal">				
+						SENEGAL	</a></li>
+						
+							
+				<li><a href="https://www.ecowas.int/member-states/sierra-leone/">
+						<img width ="75" height="75" src="flags/sierraLeone.jpg" alt="sierra-leone">				
+						SIERRA LEONE </a>
+				</li>
+						
+							
+				<li><a href="https://www.ecowas.int/member-states/togo/">
+						<img width="75" height="75" src="flags/togo.jpg" alt="Togo">				
+						TOGO				</a>
+				</li>
+				</ul>		
+			</div>
+	</div>
+	
+</div>
+
+
+
+
+
+<footer>
+   <nav style ="background-color:#70AD47; text-align:right;">
+	        <a class="btn" href="#">English</a>
+		    <a class="btn" href="#">French</a>
+		    <a class="btn" href="#">Português</a>
+			</nav>
+
+</footer>
+
+
+
+ 
+<script src ="js/node_modules/jquery/dist/jquery.min.js"></script>
+
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+ 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+
+<script src ="js/node_modules/bootstrap/js/bootstrap.min.js"></script>
+ 
+   </body>
+
+</html>
