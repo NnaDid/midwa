7ef8ba95401191c5ba38a2c8b8c0acc7ae67953b
Akoji Timothy <46170255+CodeMaestro1000@users.noreply.github.com>
2019-11-26 11:30:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -178,7 +178,7 @@
 			  </div>
 			</div>
 			</div>
-			<div class ="col-md-4">	
+			<div class ="col-md-4 mt-2">	
 			<div class="card" style="border-color:#70AD47;">
 			  <div class="card-body">
 				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
